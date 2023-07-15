875a2ed1e053567f16404b55b0ff11dfab6eeb65
klevi43 <75236133+klevi43@users.noreply.github.com>
2023-07-15 19:44:07 +0900

    Add files via upload
    Fixed issue with app reverting after button press.

--- main.py
+++ main.py
@@ -5,130 +5,140 @@ import numpy as np
 
 API_KEY = "e6bec0163bf29d88088cb3d22b06bbae"
 
+
 @st.cache_data
 def fetch_weather_data(city, unit):
-    unit_code = 'metric' if unit == 'Celsius' else 'imperial'
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units={unit_code}&appid={API_KEY}"
-    response = requests.get(url)
-    weather_data = response.json()
+    try:
+        unit_code = 'metric' if unit == 'Celsius' else 'imperial'
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units={unit_code}&appid={API_KEY}"
+        response = requests.get(url)
+        weather_data = response.json()
 
-    general = weather_data['weather'][0]['main']
-    icon_id = weather_data['weather'][0]['icon']
-    icon = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
+        general = weather_data['weather'][0]['main']
+        icon_id = weather_data['weather'][0]['icon']
+        icon = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
+
+    except Exception:
+        st.error("Failed to fetch the data. Please check if the city name is correct.")
+        return None, None, None
 
     return weather_data, general, icon
 
+
 st.title("Weather App")
 
-st.sidebar.header("Input Options")
+# st.sidebar.header("Input Options")
+with st.form(key="my_form"):
+    city = st.text_input("City name", "London") # find a way to detect states
+    unit = st.radio('Unit of temperature', ('Celsius', 'Fahrenheit'))
 
-city = st.sidebar.text_input("City name", "London")
-unit = st.sidebar.radio('Unit of temperature', ('Celsius', 'Fahrenheit'))
+    button = st.form_submit_button("Fetch Weather Data")
 
-button = st.sidebar.button("Fetch Weather Data")
 
-if button:
-    data, general, icon = fetch_weather_data(city, unit)
-    if data['cod'] != 200:
-        st.error("Failed to fetch the data. Please check if the city name is correct.")
-    else:
-        weather_data = {
-            "Temperature": data['main']['temp'],
-            "Feels Like": data['main']['feels_like'],
-            "Temperature Min": data['main']['temp_min'],
-            "Temperature Max": data['main']['temp_max'],
-            "Pressure": data['main']['pressure'],
-            "Humidity": data['main']['humidity'],
-            "Wind Speed": data['wind']['speed'],
-            "General": general
-        }
-        df = pd.DataFrame(weather_data, index=[0])
+    if button:
+        data, general, icon = fetch_weather_data(city, unit)
+        # if data['cod'] != 200:
+        #    st.error("Failed to fetch the data. Please check if the city name is correct.")
+        if data and general and icon is not None:
+            # Success Box
+            st.success("Weather data fetched successfully!")
+            # Info Box
+            st.info(f"This is the weather information for {str.title(city)}.")
 
-        # Display Weather Icon
-        st.image(icon, caption='Current Weather Icon')
+            st.header("Weather Data")
 
-        # Interactive Table
-        st.dataframe(df)
+            weather_data = {
+                "Temperature": data['main']['temp'],
+                "Feels Like": data['main']['feels_like'],
+                "Temperature Min": data['main']['temp_min'],
+                "Temperature Max": data['main']['temp_max'],
+                "Pressure": data['main']['pressure'],
+                "Humidity": data['main']['humidity'],
+                "Wind Speed": data['wind']['speed'],
+                "General": general
+            }
+            df = pd.DataFrame(weather_data, index=[0])
 
-        # Bar chart
-        st.bar_chart(df.drop(columns=['General']).T)  # Transpose the dataframe
 
-        # Map
-        coordinates_df = pd.DataFrame({'lat': [data['coord']['lat']], 'lon': [data['coord']['lon']]})
-        st.map(coordinates_df)
 
-        # Info Box
-        st.info(f"This is the weather information for {city}.")
+            # Display Weather Icon
+            st.image(icon, caption='Current Weather Icon')
 
-        # Success Box
-        st.success("Weather data fetched successfully!")
+            # Interactive Table
+            st.dataframe(df)
 
-        # Checkbox
-        if st.checkbox('Show raw data'):
-            st.write(df)
+            # Bar chart
+            st.bar_chart(df.drop(columns=['General']).T)  # Transpose the dataframe
 
-        # Selectbox
-        selected_feature = st.selectbox('Select feature', list(df.columns))
+            # Map
+            coordinates_df = pd.DataFrame({'lat': [data['coord']['lat']], 'lon': [data['coord']['lon']]})
+            st.map(coordinates_df)
 
-        if selected_feature:
-            st.line_chart(df[selected_feature])
+            # Checkbox
+            if st.checkbox('Show raw data'):
+                st.write(df)
 
-        # Multiselect
-        selected_features = st.multiselect('Select features', list(df.columns), default=list(df.columns))
+            # Selectbox
+            selected_feature = st.selectbox('Select feature', list(df.columns))
 
-        if len(selected_features) > 0:
-            st.area_chart(df[selected_features])
+            if selected_feature:
+                st.line_chart(df[selected_feature])
 
-        # Slider
-        selected_temperature = st.slider('Select a range of temperature', 0, 100, (25, 75))
+            # Multiselect
+            selected_features = st.multiselect('Select features', list(df.columns), default=list(df.columns))
 
-        # Select-slider
-        hours = st.select_slider('Select a range of hours', options=range(24))
+            if len(selected_features) > 0:
+                st.area_chart(df[selected_features])
 
-        # File Uploader
-        file = st.file_uploader("Upload an image of the city (optional)")
+            # Slider
+            selected_temperature = st.slider('Select a range of temperature', 0, 100, (25, 75))
 
-        # Color Picker
-        color = st.color_picker("Pick a color", "#00f900")
+            # Select-slider
+            hours = st.select_slider('Select a range of hours', options=range(24))
 
-        # Text-area
-        comment = st.text_area("Leave a comment here", "")
+            # File Uploader
+            file = st.file_uploader("Upload an image of the city (optional)")
 
-        # Progress Bar
-        import time
-        latest_iteration = st.empty()
-        bar = st.progress(0)
-        for i in range(100):
-            latest_iteration.text(f'Progress {i+1}')
-            bar.progress(i + 1)
-            time.sleep(0.01)
+            # Color Picker
+            color = st.color_picker("Pick a color", "#00f900")
 
-        # Columns
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.button("Good")
-        with col2:
-            st.button("Better")
-        with col3:
-            st.button("Best")
+            # Text-area
+            comment = st.text_area("Leave a comment here", "")
 
-        # Expander
-        with st.expander("See explanation"):
-            st.write("""
-            The data in the table is fetched from OpenWeatherMap API.
-            It shows the current weather condition in the selected city.
-            """)
+            # Progress Bar
+            import time
 
-st.header("Weather Data")
+            latest_iteration = st.empty()
+            bar = st.progress(0)
+            for i in range(100):
+                latest_iteration.text(f'Progress {i + 1}')
+                bar.progress(i + 1)
+                time.sleep(0.01)
+
+            # Columns
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.button("Good")
+            with col2:
+                st.button("Better")
+            with col3:
+                st.button("Best")
+
+            # Expander
+            with st.expander("See explanation"):
+                st.write("""
+                The data in the table is fetched from OpenWeatherMap API.
+                It shows the current weather condition in the selected city.
+                """)
+
+
 
 # Example time series data
-time_index = pd.date_range('2023-01-01', periods=100)
-time_series_df = pd.DataFrame(np.random.randn(100, 2), index=time_index, columns=['A', 'B'])
+# time_index = pd.date_range('2023-01-01', periods=100)
+# time_series_df = pd.DataFrame(np.random.randn(100, 2), index=time_index, columns=['A', 'B'])
 
 # Line chart
-st.line_chart(time_series_df['A'])
+# st.line_chart(time_series_df['A'])
 
 # Area chart
-st.area_chart(time_series_df)
-
-
+# st.area_chart(time_series_df)
